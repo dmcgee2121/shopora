2bb163534332e767fe9288a6d1b2a084512dcad6
Mayhem1017 <dmcgee2121@gmail.com>
2026-05-12 20:36:56 -0500

    Fix missing Link import

--- src/pages/admin/AdminCustomersPage.jsx
+++ src/pages/admin/AdminCustomersPage.jsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import AdminPageHeader from '../../components/admin/AdminPageHeader';
 import { useAuth } from '../../context/AuthContext';
 import { useOrders } from '../../context/OrdersContext';
